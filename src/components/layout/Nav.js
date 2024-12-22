31cc2ab1bdba50a08e968e9f2f9397366fe79750
LacamJC <joaoramajo744@gmail.com>
2024-12-22 00:19:58 -0300

    fix menu

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -3,25 +3,27 @@ import styles from '../../scss/components/Nav.module.css'
 import Header from "./Header"
 function Nav(){
 
-
+    function closeNav(){
+      document.getElementById('checkbox').checked = false
+    }
 
     return(
         <>
       <Header/>
       <nav className={styles.nav_menu}>
-          <input type="checkbox" className={styles.check}></input>
+          <input type="checkbox" id="checkbox" className={styles.check}></input>
           <ul className={styles.nav_menu__list}>
                 <li className={styles.nav_menu__item}>
-                  <Link to="/" className={styles.nav_menu__link} aria-current="page">Home</Link>
+                  <Link to="/" className={styles.nav_menu__link} onClick={closeNav} aria-current="page">Home</Link>
                 </li>
                 <li className={styles.nav_menu__item}>
-                <Link to="/CadastrarUsuario" className={styles.nav_menu__link} >Cadastrar usuarios</Link>
+                <Link to="/CadastrarUsuario" onClick={closeNav} className={styles.nav_menu__link} >Cadastrar usuarios</Link>
                 </li>
                 <li className={styles.nav_menu__item}>
-                  <Link className={styles.nav_menu__link} to="/ListaUsuario">Lista de usuarios</Link>
+                  <Link className={styles.nav_menu__link} onClick={closeNav} to="/ListaUsuario">Lista de usuarios</Link>
                 </li>
                 <li className={styles.nav_menu__item}>
-                  <Link className={styles.nav_menu__link} to="/Login">Login</Link>
+                  <Link className={styles.nav_menu__link} onClick={closeNav} to="/Login">Login</Link>
                 </li>
           </ul>
         </nav>
